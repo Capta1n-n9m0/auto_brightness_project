9dab5f763f53bd7ed2c81289d774ce13e06a7b53
Capta1n-n9m0 <alievabbas1@gmail.com>
2022-05-25 20:11:47 +0400

    Defined all needed types

--- bmp_core.h
+++ bmp_core.h
@@ -4,12 +4,48 @@
 #include <inttypes.h>
 #include <stdlib.h>
 
-typedef BMP_HEADER;
-typedef DIB_HEADER;
-typedef BIT_MASKS;
-typedef COLOR_TABLE;
-typedef IMAGE_DATA;
-typedef COLOR_PROFILE;
+struct BMP_HEADER_{
+    uint8_t signature[2];
+    uint32_t size;
+    uint8_t application_reserved[4];
+    uint32_t offset;
+};
+typedef struct BMP_HEADER_ BMP_HEADER;
+
+struct DIB_HEADER_{
+    uint32_t header_size;
+    uint32_t width;
+    uint32_t height;
+    uint16_t c_color_planes;
+    uint16_t bits_per_pixels;
+    uint8_t not_important[24];
+};
+typedef struct DIB_HEADER_ DIB_HEADER;
+
+/// not implemented
+typedef char BIT_MASKS;
+
+/// not implemented
+typedef char COLOR_TABLE;
+
+struct PIXEL__{
+    uint8_t r, g, b, alpha;
+};
+union PIXEL_{
+    struct PIXEL__ components;
+    uint32_t value;
+    uint8_t bytes[4];
+};
+typedef union PIXEL_ PIXEL;
+
+struct IMAGE_DATA_{
+    uint32_t width, height;
+    PIXEL **data;
+};
+typedef char IMAGE_DATA;
+
+/// not implemented
+typedef char COLOR_PROFILE;
 
 struct BMP_FILE_{
     BMP_HEADER *bmpHeader;
